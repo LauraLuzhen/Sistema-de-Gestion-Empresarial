0291dfffcf1c1e4ea08a51c1e8be190f25513f58
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-10-08 14:10:19 +0200

    ejercicios

---
+++ Tema5/HolaMundo/HolaMundo/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HolaMundo
+{
+    public partial class _Default : Page
+    {
+        string nombre = txtNombre.Text.Trim();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullorEmpty(nombre))
+            {
+                lblSaludo.Text = "ERROR";
+            }
+            else
+            {
+                lblSaludo.Text = "Hola, " + txtNombre.Text + "!";
+            }
+        }
+    }
+}
