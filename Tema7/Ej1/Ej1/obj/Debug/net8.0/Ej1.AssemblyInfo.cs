35da78957626633439abfcda0ef21dbccd8247fa
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-10-27 14:42:37 +0100

    a

--- Tema7/Ej1/Ej1/obj/Debug/net8.0/Ej1.AssemblyInfo.cs
+++ Tema7/Ej1/Ej1/obj/Debug/net8.0/Ej1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6b5383644014c82b0259bbf4c110f739ecb9b25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555da12af08c66b7a33690d416d86791d20757bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ej1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
